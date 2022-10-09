35f7d0169e5410820f29d7e9835dc8c370bfc882
Brendi <brendan.prednis@protonmail.com>
2022-10-08 20:47:16 -0700

    updated packages and added graphql dir

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+let cache = new InMemoryCache();
+
+// APOLLO CLIENT
+let client = new ApolloClient({
+    cache,
+    uri: "uri goes here",
+    name: "yula-website",
+    version: "1.2",
+    queryDeduplication: false,
+    headers: {
+        Authorization: "Bearer API_TOKEN_HERE",
+    },
+
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: "cache-and-network",
+        },
+    },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
