7c7e1a32ec1ba1763d60c2d4cba4daa20754b3d2
Brendi <brendan.prednis@protonmail.com>
2022-10-08 20:26:34 -0700

    added spotify links instead of custom links

--- pages/music.tsx
+++ pages/music.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from "next";
 import Layout from "../components/Layout";
-import _Layout from "../components/Layout";
+
+// components
+import { Box, Flex } from "@chakra-ui/react";
 
 export interface IMusicProps {
     songTitle: string;
@@ -9,53 +11,28 @@ export interface IMusicProps {
     artistTitle: string;
 }
 
-const musicTabs: IMusicProps[] = [
-    {
-        songTitle: "Everything You Feel",
-        songLink:
-            "https://open.spotify.com/track/7r4cZWIXy6ChgO9arBIfGA?si=564874bd3abc4094",
-        songImageURL:
-            "https://i.scdn.co/image/ab67616d00001e0235547783541b0e0ef45490af",
-        artistTitle: "Yula",
-    },
-    {
-        songTitle: "A Narrow Road",
-        songLink:
-            "https://open.spotify.com/track/3rRuewnnd90C05vlLhUuRE?si=b3d6d914a2454a9f",
-        songImageURL:
-            "https://i.scdn.co/image/ab67616d00001e025c22c36fef94a544fc403f64",
-        artistTitle: "Yula",
-    },
-    {
-        songTitle: "Final Level",
-        songLink:
-            "https://open.spotify.com/track/2yd4gwwXzruCjErTbOPgCv?si=ecb0e05a7c2045a8",
-        songImageURL:
-            "https://i.scdn.co/image/ab67616d00001e0246ccb842e55fddc1307a67a6",
-        artistTitle: "Yula",
-    },
-];
-
-// components
-import { Box, Grid } from "@chakra-ui/react";
-import MusicCard from "../components/MusicCard";
 function MusicPage() {
     return (
         <Layout>
-            <Box background="black" alignItems="center">
-                <Grid
-                    p={2}
-                    paddingTop={12}
-                    gridTemplateRows="repeat(4,1fr)"
-                    alignItems="center"
-                    textAlign="left"
-                    width="100%"
-                >
-                    {musicTabs.map((music: IMusicProps) => (
-                        <MusicCard key={music.songTitle} {...music} />
-                    ))}
-                </Grid>
-            </Box>
+            <Flex
+                alignItems="center"
+                justifyContent="center"
+                background="black"
+                width="100%"
+                padding="2rem"
+            >
+                <Box justifyContent="center" alignItems="center" width="50%">
+                    <iframe
+                        style={{ borderRadius: "12px" }}
+                        src="https://open.spotify.com/embed/artist/32GQyTofHn92DwudAFpVf8?utm_source=generator&theme=0"
+                        width="100%"
+                        height="380"
+                        frameBorder="0"
+                        allowFullScreen={false}
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    ></iframe>
+                </Box>
+            </Flex>
         </Layout>
     );
 }
